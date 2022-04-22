102062d7cd20adae3d9f6896588936b8405c5514
Joshuaboyi <boyijoshua72@gmail.com>
2022-04-22 05:21:46 +0100

    cham

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const product = require("./api/pk");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/pk", product);
+app.use("/", product);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
